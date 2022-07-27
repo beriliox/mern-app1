aabc3a3d29da08b6f2367f1a6c31290037d7b30e
Carlo <carloecheverriafuentes.services@gmail.com>
2022-07-27 11:46:03 -0400

    #2: Update server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const app = express();
 // DB connection
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb://mongo:27018/ce-mern-app1", { useNewUrlParser: true })
+  .connect("mongodb://mongo:27017/ce-mern-app1", { useNewUrlParser: true })
   .then(() => console.log("DB connected"))
   .catch((err) => console.log(err));
 
@@ -13,19 +13,19 @@ mongoose
 const Coaster = require("./Models/Coaster.model");
 
 // Routing
-app.get("/", (req, res) =>
+app.get("/", (req, res) => {
   res.json({
     message: "Hello World!",
     date: new Date().toISOString(),
     campus: "Santiago",
-  })
-);
+  });
+});
 
-app.get("/api/v1/coasters", (req, res) =>
+app.get("/api/v1/coasters", (req, res) => {
   Coaster.find()
     .then((coasters) => res.json(coasters))
-    .catch((err) => res.json(err))
-);
+    .catch((err) => res.json(err));
+});
 
 app.get("/api/v1/coaster/:coaster_id", (req, res) => {
   const { coaster_id } = req.params;
